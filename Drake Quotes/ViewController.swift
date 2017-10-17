7e4d7d4b776327cfc9766584f963067ab0c18e6e
Patrick Conrey <patrickconrey@gmail.com>
2017-10-17 19:00:11 -0400

    Initial Commit

---
+++ Drake Quotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drake Quotes
+//
+//  Created by Patrick Conrey on 10/17/17.
+//  Copyright © 2017 ConreyDesigns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
